2b30e268242131d458167d9bfeae1434c69672aa
Alberto Acerbis <alberto.acerbis@gmail.com>
2026-05-05 20:19:22 +0200

    Try to integrate Kurrent

--- src/BrewApi/BrewUp.AppHost/AppHost.cs
+++ src/BrewApi/BrewUp.AppHost/AppHost.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var brewUpApi = builder.AddProject<Projects.BrewUp_Rest>("brewUpApi");
+var kurrent = builder.AddKurrentDB("kurrentDb")
+    .WithEnvironment("KURRENTDB_ENABLE_ATOM_PUB_OVER_HTTP", "true")
+    .WithDataVolume("brewup-kurrent-data");
+var brewUpApi = builder.AddProject<Projects.BrewUp_Rest>("brewUpApi")
+    .WithReference(kurrent)
+    .WaitFor(kurrent)
+    .WithEnvironment("BrewUp__EventStore__ConnectionString", kurrent.Resource.ConnectionStringExpression);
 
 builder.Build().Run();
