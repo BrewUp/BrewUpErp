8e70ad3297644634760325025d4f01fcfbf87b4f
Alberto Acerbis <alberto.acerbis@gmail.com>
2026-05-05 19:05:00 +0200

    Remove settings

---
+++ src/BrewSpa/BrewSpa.Shared/Tests/ModulesProjectUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace BrewSpa.Shared.Tests;
+
+public static class ModulesProjectUtils
+{
+  private static readonly string[] SolutionProjects = [
+        "BrewSpa.Dashboards.Facade",
+        "BrewSpa.Dashboards.Services",
+        "BrewSpa.Dashboards.Tests",
+
+        "BrewSpa.MasterData.Facade",
+        "BrewSpa.MasterData.Services",
+        "BrewSpa.MasterData.Tests",
+
+        "BrewSpa.Sales.Facade",
+        "BrewSpa.Sales.Services",
+        "BrewSpa.Sales.Tests"
+    ];
+
+  public static IEnumerable<string> GetModuleProjects(bool includeFacadeProjects, IEnumerable<string> excludeModules)
+  {
+    return SolutionProjects
+        .Where(project =>
+            (includeFacadeProjects || !project.EndsWith(".Facade")) &&
+            !excludeModules.Any(module => project.StartsWith($"BrewSpa.{module}.")));
+  }
+}
