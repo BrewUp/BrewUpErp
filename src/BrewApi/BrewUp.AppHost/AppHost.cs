d839dc204006550bab283bedebbc3ed2d73af7c9
Alberto Acerbis <alberto.acerbis@gmail.com>
2026-05-07 14:57:23 +0200

    AppHost

--- src/BrewApi/BrewUp.AppHost/AppHost.cs
+++ src/BrewApi/BrewUp.AppHost/AppHost.cs
@@ -1,11 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var kurrent = builder.AddKurrentDB("kurrentDb")
-    .WithEnvironment("KURRENTDB_ENABLE_ATOM_PUB_OVER_HTTP", "true")
-    .WithDataVolume("brewup-kurrent-data");
-var brewUpApi = builder.AddProject<Projects.BrewUp_Rest>("brewUpApi")
-    .WithReference(kurrent)
-    .WaitFor(kurrent)
-    .WithEnvironment("BrewUp__EventStore__ConnectionString", kurrent.Resource.ConnectionStringExpression);
+// // var kurrent = builder.AddKurrentDB("kurrentDb")
+// //     .WithEnvironment("KURRENTDB_ENABLE_ATOM_PUB_OVER_HTTP", "true")
+// //     .WithDataVolume("brewup-kurrent-data");
+// var brewUpApi = builder.AddProject<Projects.BrewUp_Rest>("brewUpApi")
+//     // .WithReference(kurrent)
+//     // .WaitFor(kurrent)
+//     // .WithEnvironment("BrewUp__EventStore__ConnectionString", kurrent.Resource.ConnectionStringExpression);
 
 builder.Build().Run();
