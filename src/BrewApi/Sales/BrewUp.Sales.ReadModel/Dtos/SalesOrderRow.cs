bd3efa6e7e5ea76781596f79509bd9d78a8dd819
Alberto Acerbis <alberto.acerbis@gmail.com>
2026-05-13 11:35:33 +0200

    Resolve bugs

--- src/BrewApi/Sales/BrewUp.Sales.ReadModel/Dtos/SalesOrderRow.cs
+++ src/BrewApi/Sales/BrewUp.Sales.ReadModel/Dtos/SalesOrderRow.cs
@@ -1,4 +1,5 @@
 ﻿using BrewUp.Shared.CustomTypes;
+using BrewUp.Shared.DomainIds;
 using BrewUp.Shared.ExternalContracts;
 using BrewUp.Shared.ExternalContracts.Sales;
 
@@ -10,6 +11,21 @@ public class SalesOrderRow
     public string BeerName { get; private set; } = string.Empty;
     public Quantity Quantity { get; private set; } = default!;
     public Price Price { get; private set; } = default!;
+    
+    protected SalesOrderRow()
+    {}
+    
+    internal static SalesOrderRow Create(BeerId beerId, BeerName beerName, Quantity quantity, Price price)
+        => new SalesOrderRow(beerId.Value, beerName.Value, new Quantity(quantity.Value, quantity.UnitOfMeasure),
+            new Price(price.Value, price.Currency));
+    
+    private SalesOrderRow(string beerId, string beerName, Quantity quantity, Price price)
+    {
+        BeerId = beerId;
+        BeerName = beerName;
+        Quantity = quantity;
+        Price = price;
+    }
 
     internal SalesOrderRowJson ToJson => new()
     {
